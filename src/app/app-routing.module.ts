c8c85d3252b62906d787ec333e0afc1b333a7dfa
carrillocarlosce <bestguru.peru@gmail.com>
2018-08-10 20:18:34 -0500

    Merge Ramle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent }   from './home/home.component';
+import { HomeComponent }   from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: DashboardComponent }
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
