749b72e4f2cefd0c91b84d4a5fedd7ae59ecbdc6
carrillocarlosce <bestguru.peru@gmail.com>
2018-08-28 12:39:31 -0500

    mejoras en plan y summary

--- src/app/subscription/plan/plan.component.ts
+++ src/app/subscription/plan/plan.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 
 @Component({
@@ -11,11 +13,38 @@ export class PlanComponent implements OnInit {
   cat:Array<String>;
   suscriptor:any;
   showForm:boolean;
-  constructor() { 
+  plans:Array<Object>;
+  tamanos:Array<Object>;
+  tipos:Array<Object>;
+  susciption:Object;
+  alert:String;
+
+  constructor(
+  	private route: ActivatedRoute,
+    private router: Router
+    ) { 
   	this.suscriptor = {nombre:'',ryp:'',cdir:'',dir:'',ciudad:'',tel:'' }
   	this.parentesco = ["Primo", "Prima", "Cliente", "Amiga" , "Amigo", "Novio", "Novia", "Abuela", "Abuelo", "Mamá", "Papá", "Hermana", "Hermano", "Hijo", "Hija", "Tío", "Tía", "Esposa", "Esposo"];
   	this.cat = ["Casa", "Oficina", "Otro"];
   	this.showForm=true;
+  	this.plans = [
+  	{_id: 1, nombre:"FRESCAS DE CULTIVO", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"},
+  	{_id: 2, nombre:"FRESCAS DE CULTIVO", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"},
+  	{_id: 3, nombre:"FRESCAS DE CULTIVO", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"},
+  	{_id: 4, nombre:"FRESCAS DE CULTIVO", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"}
+  	];
+  	this.tamanos = [
+  	{_id: 1, nombre:"ORIGINAL", desc: "<strong>10 a 15 tallos</strong>  cuidadosamente seleccionados", icon:"assets/imgs/plans/size-original.png"},
+  	{_id: 2, nombre:"ORIGINAL", desc: "<strong>10 a 15 tallos</strong>  cuidadosamente seleccionados", icon:"assets/imgs/plans/size-original.png"},
+  	{_id: 3, nombre:"ORIGINAL", desc: "<strong>10 a 15 tallos</strong>  cuidadosamente seleccionados", icon:"assets/imgs/plans/size-original.png"}
+  	];
+  	this.tipos = [
+  	{_id: 1, nombre:"SEMANAL", desc: "<strong>12 a 15 tallos</strong>  cuidadosamente seleccionados", img:"assets/img/calendar_icon_grey.png"},
+  	{_id: 2, nombre:"QUINCENAL", desc: "<strong>De 12 a 15 tallos</strong> cuidadosamente seleccionados", img:"assets/img/calendar_icon_grey.png"},
+  	{_id: 3, nombre:"MENSUAL", desc: "<strong>De 12 a 15 tallos</strong> cuidadosamente seleccionados", img:"assets/img/calendar_icon_grey.png"}
+  	];
+  	this.susciption= {plan:0,tamano:0,tipo:0};
+  	this.alert = "";
   }
 
   ngOnInit() {
@@ -23,5 +52,34 @@ export class PlanComponent implements OnInit {
 
   addSuscriptor(){
   	this.showForm = false;
+  }
+  addPlan(id){
+  	this.susciption["plan"] = id;
+  }
+  addTamano(id){
+  	this.susciption["tamano"] = id;
+  }
+  addTipo(id){
+  	this.susciption["tipo"] = id;
+  }
+
+  goToSummary(){
+  	if(!this.showForm && this.susciption["tipo"] != 0 && this.susciption["plan"] != 0 && this.susciption["tamano"] != 0){
+  		this.alert = "";
+  		// {nombre:'',ryp:'',cdir:'',dir:'',ciudad:'',tel:'' }
+  		this.router.navigate(['subscription/summary'], { queryParams: { 
+  			nombre:  this.suscriptor['nombre'],
+  			ryp: this.suscriptor['nombre'],
+  			cdir: this.suscriptor['cdir'],
+  			dir: this.suscriptor['dir'],
+  			ciudad: this.suscriptor['ciudad'],
+  			tel: this.suscriptor['tel'],
+  			plan: this.susciption['plan'],
+  			tam: this.suscriptor['tamano'],
+  			tipo: this.susciption['tipo']
+  			} });
+  	}else{
+  		this.alert = "Debe completar todos los datos";
+  	}
   }
 }
--- src/app/subscription/summary/summary.component.ts
+++ src/app/subscription/summary/summary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent implements OnInit {
-
-  constructor() { }
+  suscriptor:Object;
+  plan:String;
+  tam:String;
+  tipo:String;
+  constructor(
+  	private route: ActivatedRoute,
+  	) {
+  	this.suscriptor = {nombre:'',ryp:'',cdir:'',dir:'',ciudad:'',tel:'' }
+  }
 
   ngOnInit() {
+  	this.route
+      .queryParams
+      .subscribe(params => {
+        // Defaults to 0 if no query param provided.
+        this.suscriptor['nombre'] = params['nombre'];
+        this.suscriptor['ryp'] = params['ryp'];
+        this.suscriptor['cdir'] = params['cdir'];
+        this.suscriptor['dir'] = params['dir'];
+        this.suscriptor['ciudad'] = params['ciudad'];
+        this.suscriptor['tel'] = params['tel'];
+        this.plan = "FRESCAS DE CULTIVO";
+        this.tam = params['tam'];
+        this.tipo = params['tipo'];
+      })
   }
 
 }
