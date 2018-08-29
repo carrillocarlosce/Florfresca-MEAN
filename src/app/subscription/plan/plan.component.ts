c26fe8c7520ec7cb33b1fc0708037ac8513ce236
carrillocarlosce <bestguru.peru@gmail.com>
2018-08-28 22:44:51 -0500

    cambios en suscripciones

--- src/app/subscription/plan/plan.component.ts
+++ src/app/subscription/plan/plan.component.ts
@@ -29,19 +29,19 @@ export class PlanComponent implements OnInit {
   	this.showForm=true;
   	this.plans = [
   	{_id: 1, nombre:"FRESCAS DE CULTIVO", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"},
-  	{_id: 2, nombre:"FRESCAS DE CULTIVO", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"},
-  	{_id: 3, nombre:"FRESCAS DE CULTIVO", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"},
-  	{_id: 4, nombre:"FRESCAS DE CULTIVO", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"}
+  	{_id: 2, nombre:"ROSAS", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"},
+  	{_id: 3, nombre:"CLÁSICAS", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"},
+  	{_id: 4, nombre:"EXÓTICAS", desc: "La más cuidadosa selección de tallos de rosas frescas de nuestro cultivo", img:"assets/imgs/item-4.jpg"}
   	];
   	this.tamanos = [
   	{_id: 1, nombre:"ORIGINAL", desc: "<strong>10 a 15 tallos</strong>  cuidadosamente seleccionados", icon:"assets/imgs/plans/size-original.png"},
-  	{_id: 2, nombre:"ORIGINAL", desc: "<strong>10 a 15 tallos</strong>  cuidadosamente seleccionados", icon:"assets/imgs/plans/size-original.png"},
-  	{_id: 3, nombre:"ORIGINAL", desc: "<strong>10 a 15 tallos</strong>  cuidadosamente seleccionados", icon:"assets/imgs/plans/size-original.png"}
+  	{_id: 2, nombre:"DELUXE", desc: "<strong>18 a 14 tallos</strong>  cuidadosamente seleccionados", icon:"assets/imgs/plans/size-delux.png"},
+  	{_id: 3, nombre:"GRANDE", desc: "<strong>36 a 48 tallos</strong>  cuidadosamente seleccionados", icon:"assets/imgs/plans/size-original.png"}
   	];
   	this.tipos = [
-  	{_id: 1, nombre:"SEMANAL", desc: "<strong>12 a 15 tallos</strong>  cuidadosamente seleccionados", img:"assets/img/calendar_icon_grey.png"},
-  	{_id: 2, nombre:"QUINCENAL", desc: "<strong>De 12 a 15 tallos</strong> cuidadosamente seleccionados", img:"assets/img/calendar_icon_grey.png"},
-  	{_id: 3, nombre:"MENSUAL", desc: "<strong>De 12 a 15 tallos</strong> cuidadosamente seleccionados", img:"assets/img/calendar_icon_grey.png"}
+  	{_id: 1, nombre:"SEMANAL", desc: "<strong>12 a 15 tallos</strong>  cuidadosamente seleccionados", img:"assets/imgs/icons/icon-flores.png"},
+  	{_id: 2, nombre:"QUINCENAL", desc: "<strong>De 12 a 15 tallos</strong> cuidadosamente seleccionados", img:"assets/imgs/icons/icon-flores.png"},
+  	{_id: 3, nombre:"MENSUAL", desc: "<strong>De 12 a 15 tallos</strong> cuidadosamente seleccionados", img:"assets/imgs/icons/icon-flores.png"}
   	];
   	this.susciption= {plan:0,tamano:0,tipo:0};
   	this.alert = "";
