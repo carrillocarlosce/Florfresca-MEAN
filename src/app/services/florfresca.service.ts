2346c875b8239d50e0f4dececdbb5a7efb58f4cb
carrillocarlosce <bestguru.peru@gmail.com>
2018-09-05 16:44:37 -0500

    Cambios pedidos por tato

--- src/app/services/florfresca.service.ts
+++ src/app/services/florfresca.service.ts
@@ -1,12 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 
+import { Plan } from '../models/plan';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlorfrescaService {
-
 	private heroesUrl = 'api-v1'; 
-  	constructor() { }
+  	constructor(
+  		private http: HttpClient
+  	) { }
+
+  // getHeroes (): Observable<Hero[]> {
+  //   return this.http.get<Hero[]>(this.heroesUrl)
+  //     .pipe(
+  //       tap(heroes => this.log('fetched heroes')),
+  //       catchError(this.handleError('getHeroes', []))
+  //     );
+  // }
 }
